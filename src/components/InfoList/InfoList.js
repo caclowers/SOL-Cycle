38d3af738e9351394f8338e39abf797507c2cba2
Casey <caclowers@gmail.com>
2018-10-01 21:06:14 -0500

    made some design changes, not yet complete

--- src/components/InfoList/InfoList.js
+++ src/components/InfoList/InfoList.js
@@ -4,186 +4,124 @@ class InfoList extends Component {
 
 
 
-   render() {
-      return (
-         <h2 >
-            <div className="sectionHeader">
-               <h2 className="h2ToCenter">0 to 2: Low</h2>
-            </div>
+  render() {
+    return (
+      <div >
 
-            A UV Index reading of 0 to 2 means LOW RISK for the average person.
-            < br />
-            <br />
-            <ul>
-               <li >
-                  Wear sunglasses on bright days.
-               </li>
-               <br />
-               <br />
-               <li>
-                  If you burn easily, cover up and use broad spectrum SPF 30+ sunscreen.
-               </li>
-               <br />
-               <br />
-               <li>
-                  Watch out for bright surfaces, like sand, water and snow, which reflect UV and increase exposure.
-               </li>
-            </ul>
-            <br />
-            <br />
-            <br />
-            <div className="sectionHeader">
-               <h2 className="h2ToCenter">3 to 5: Moderate</h2>
-            </div>
 
-            A UV Index reading of 3 to 5 means MODERATE RISK of harm from unprotected sun exposure.
-            < br />
-            < br />
-            <ul>
-               <li>
-                  Stay in shade near midday when the sun is strongest.
-               </li>
-               <br />
-               <br />
-               <li>
-                  If outdoors, wear protective clothing, a wide-brimmed hat, and UV-blocking sunglasses.
-               </li>
-               <br />
-               <br />
-               <li>
-                  Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after swimming or sweating.
-               </li>
-               <br />
-               <br />
-               <br />
+        <div className="sectionHeader">
+          <h2 className="h2ToCenter">0 to 2: Low</h2>
+        </div>
 
-               <li>
-                  Watch out for bright surfaces, like sand, water and snow, which reflect UV and increase exposure.
-               </li>
-            </ul>
-            <br />
-            <br />
-            <br />
-            <div className="sectionHeader">
-               <h2 className="h2ToCenter">6 to 7: High</h2>
-            </div>
+        A UV Index reading of 0 to 2 means LOW RISK for the average person.
 
-            A UV Index reading of 6 to 7 means HIGH RISK of harm from unprotected sun exposure.< br />
-            Protection against skin and eye damage is needed.< br />
-            <br />
-            <ul>
-               <li>
-                  Reduce time in the sun between 10 a.m. and 4 p.m.
+        <ul>
+          <li >
+            Wear sunglasses on bright days.
+            </li>
+          <li>
+            If you burn easily, cover up and use broad spectrum SPF 30+ sunscreen.
                </li>
-               <br />
-               <br />
-               <li>
-                  If outdoors, seek shade and wear protective clothing, a wide-brimmed hat, and UV-blocking sunglasses.
+          <li>
+            Watch out for bright surfaces, like sand, water and snow, which reflect UV and increase exposure.
                </li>
-               <br />
-               <br />
-               <br />
-               <li>
-                  Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after swimming or sweating.
-               </li>
-               <br />
-               <br /><br />
+        </ul>
+        <div className="sectionHeader">
+          <h2 className="h2ToCenter">3 to 5: Moderate</h2>
+        </div>
 
-               <li>
-                  Watch out for bright surfaces, like sand, water and snow, which reflect UV and increase exposure.
+        A UV Index reading of 3 to 5 means MODERATE RISK of harm from unprotected sun exposure.
+        <ul>
+          <li>
+            Stay in shade near midday when the sun is strongest.
                </li>
-            </ul>
-            <br />
-            <br />
-            <br />
-            <div className="sectionHeader">
-               <h2 className="h2ToCenter">8 to 10: Very High</h2>
-            </div>
+          <li>
+            If outdoors, wear protective clothing, a wide-brimmed hat, and UV-blocking sunglasses.
+               </li>
+          <li>
+            Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after swimming or sweating.
+               </li>
+          <li>
+            Watch out for bright surfaces, like sand, water and snow, which reflect UV and increase exposure.
+               </li>
+        </ul>
+        <div className="sectionHeader">
+          <h2 className="h2ToCenter">6 to 7: High</h2>
+        </div>
 
-            A UV Index reading of 8 to 10 means VERY HIGH RISK of harm from unprotected sun exposure.< br />
-            Take extra precautions because unprotected skin and eyes will be damaged and can burn quickly.< br />
-            <br />
-            <ul>
-               <li>
-                  Minimize sun exposure between 10 a.m. and 4 p.m.
+        A UV Index reading of 6 to 7 means HIGH RISK of harm from unprotected sun exposure.
+        Protection against skin and eye damage is needed.
+        <ul>
+          <li>
+            Reduce time in the sun between 10 a.m. and 4 p.m.
                </li>
-               <br />
-               <br />
-               <li>
-                  If outdoors, seek shade and wear protective clothing, a wide-brimmed hat, and UV-blocking sunglasses.
+          <li>
+            If outdoors, seek shade and wear protective clothing, a wide-brimmed hat, and UV-blocking sunglasses.
                </li>
-               <br />
-               <br />
-               <br />
-               <li>
-                  Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after swimming or sweating.
+          <li>
+            Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after swimming or sweating.
                </li>
-               <br />
-               <br />
-               <br />
-               <li>
-                  Watch out for bright surfaces, like sand, water and snow, which reflect UV and increase exposure.
+          <li>
+            Watch out for bright surfaces, like sand, water and snow, which reflect UV and increase exposure.
                </li>
-            </ul>
-            <br />
-            <br />
-            <br />
-            <div className="sectionHeader">
-               <h2 className="h2ToCenter">11 or more: EXTREME</h2>
-            </div>
+        </ul>
+        <div className="sectionHeader">
+          <h2 className="h2ToCenter">8 to 10: Very High</h2>
+        </div>
 
-            A UV Index reading of 11 or more means EXTREME RISK of harm from unprotected sun exposure.< br />
-            Take all precautions because unprotected skin and eyes can burn in minutes.< br />
-            <br />
-            <ul>
-               <li>
-                  Try to AVOID sun exposure between 10 a.m. and 4 p.m.
+        A UV Index reading of 8 to 10 means VERY HIGH RISK of harm from unprotected sun exposure.
+        Take extra precautions because unprotected skin and eyes will be damaged and can burn quickly.
+        <ul>
+          <li>
+            Minimize sun exposure between 10 a.m. and 4 p.m.
                </li>
-               <br />
-               <br />
-               <li>
-                  If outdoors, seek shade and wear protective clothing, a wide-brimmed hat, and UV-blocking sunglasses.
+          <li>
+            If outdoors, seek shade and wear protective clothing, a wide-brimmed hat, and UV-blocking sunglasses.
                </li>
-               <br />
-               <br />
-               <br />
-               <li>
-                  Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after swimming or sweating.
+          <li>
+            Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after swimming or sweating.
                </li>
-               <br />
-               <br />
-               <br />
-               <li>
-                  Watch out for bright surfaces, like sand, water and snow, which reflect UV and increase exposure.
+          <li>
+            Watch out for bright surfaces, like sand, water and snow, which reflect UV and increase exposure.
+               </li>
+        </ul>
+        <div className="sectionHeader">
+          <h2 className="h2ToCenter">11 or more: EXTREME</h2>
+        </div>
+
+        A UV Index reading of 11 or more means EXTREME RISK of harm from unprotected sun exposure.
+        Take all precautions because unprotected skin and eyes can burn in minutes.
+        <ul>
+          <li>
+            Try to AVOID sun exposure between 10 a.m. and 4 p.m.
+               </li>
+          <li>
+            If outdoors, seek shade and wear protective clothing, a wide-brimmed hat, and UV-blocking sunglasses.
+               </li>
+          <li>
+            Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after swimming or sweating.
+               </li>
+          <li>
+            Watch out for bright surfaces, like sand, water and snow, which reflect UV and increase exposure.
                </li>
 
-            </ul>
-            <br />
-            <br />
-            <br />
-            <h1>The Shadow Rule</h1>
-            <br />
-            <br />
-            An easy way to tell how much UV exposure you are getting is to look for your shadow:
-            <br /><br />
-            <ul>
-               <li>
-                  If your shadow is TALLER than you are (in the early morning and late afternoon), your UV exposure is likely to be lower.
+        </ul>
+        <h1>The Shadow Rule</h1>
+        An easy way to tell how much UV exposure you are getting is to look for your shadow:
+        <ul>
+          <li>
+            If your shadow is TALLER than you are (in the early morning and late afternoon), your UV exposure is likely to be lower.
                </li>
-               <br />
-               <br />
-               <br />
-               <li>
-                  If your shadow is SHORTER than you are (around midday), you are being exposed to higher levels of UV radiation. Seek shade and protect your skin and eyes.
+          <li>
+            If your shadow is SHORTER than you are (around midday), you are being exposed to higher levels of UV radiation. Seek shade and protect your skin and eyes.
                </li>
-            </ul>
-            <br />
-            <br />
-            Information Provided by: <a href="https://www.epa.gov/sunsafety/uv-index-scale-1">epa.gov</a>
-            <button id="infoHome"><Link to="/user">Back to Home</Link></button>
-         </h2>
-      )
-   }
+        </ul>
+        
+        <button id="infoHome"><Link to="/user">Back to Home</Link></button>
+        Information Provided by: <a href="https://www.epa.gov/sunsafety/uv-index-scale-1">epa.gov</a>
+      </div>
+    )
+  }
 };
 
 export default InfoList;
--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -6,57 +6,62 @@ import { Link } from 'react-router-dom';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 
 const mapStateToProps = state => ({
-   user: state.user,
+  user: state.user,
 });
 
 
 class InfoPage extends Component {
-   componentDidMount() {
-      window.scrollTo(0, 0);
-      this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
-   }
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
+  }
 
-   componentDidUpdate() {
-      if (!this.props.user.isLoading && this.props.user.userName === null) {
-         this.props.history.push('home');
-      }
-   }
+  componentDidUpdate() {
+    if (!this.props.user.isLoading && this.props.user.userName === null) {
+      this.props.history.push('home');
+    }
+  }
 
-   shadowRule = () => {
-      window.scrollTo({
-         top: 1000000,
-         behavior: "smooth"
-      });
-   }
+  shadowRule = () => {
+    window.scrollTo({
+      top: 1000000,
+      behavior: "smooth"
+    });
+  }
 
-   render() {
-      let content = null;
+  render() {
+    let content = null;
 
-      if (this.props.user.userName) {
-         content = (
-            <div>
-               <main id="welcome">Hi,<br /> {this.props.user.userName}!</main>
-               <div className="infoDiv">
+    if (this.props.user.userName) {
+      content = (
+        <div>
+          <main id="welcome">Hi,<br /> {this.props.user.userName}!</main>
+          <div className="infoDiv">
 
-                  <div id="infoHeader">
-                     <h1>What is UV?</h1>
-                     <button id="infoHome"><Link to="/user">Back to Home</Link></button>
-                  </div>
-                 <br/>
-                  <button id="shadowLink" onClick={this.shadowRule}>THE SHADOW RULE</button>
-                  <InfoList />
-               </div>
+            <div id="infoHeader">
+              <h1>What is UV?</h1>
+              <button id="infoHome"><Link to="/user">Back to Home</Link></button>
+              <button id="shadowLink" onClick={this.shadowRule}>THE SHADOW RULE</button>
+              <h2>Ultraviolet light is measured on a 0 - 11+ scale:</h2>
+              <h2>The UV spectrum has both beneficial and harmful effets to human health.</h2>
+              <h2>UV is responsible for the formation of bone-strengthening vitamin D in humans (specifically, UVB).</h2>
+              <h2>However, suntan and sunburn are familiar effects of over-exposure of the skin to UV, along with a higher risk of SKIN CANCER.</h2>
+              
+
             </div>
-         );
-      }
-
-      return (
-         <div>
-            {/* <Nav /> */}
-            {content}
-         </div>
+            <InfoList />
+          </div>
+        </div>
       );
-   }
+    }
+
+    return (
+      <div>
+        {/* <Nav /> */}
+        {content}
+      </div>
+    );
+  }
 }
 
 // this allows us to use <App /> in index.js
